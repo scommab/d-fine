b8ea95f6c6e499078b8e2b5d2b4fd23a870bc988
Saul <scommab@gmail.com>
2012-12-19 21:40:09 -0800

    First check in

---
+++ storage.py
@@ -0,0 +1,52 @@
+import json
+
+import redis
+
+class mockRedis(object):
+  def __init__(self):
+    self.keystore = {}
+
+  def hget(self, name1, name2):
+    return self.keystore.get(name1, {}).get(name2, None)
+
+  def hset(self, name1, name2, val):
+    d = self.keystore.get(name1, {})
+    d[name2] = val
+    self.keystore[name1] = d
+
+  def hkeys(self, name1):
+    return self.keystore.get(name1, {}).keys()
+
+DEBUG = False
+    
+mock = mockRedis()
+def get_connection():
+  if DEBUG:
+    return mock
+  try:
+    return redis.Redis()
+  except:
+     #debug mode
+    return mock
+
+def get_def(name):
+  d = get_connection().hget("jargon-defs", name)
+  if not d:
+    return d
+  return json.loads(d)
+
+def get_all_def():
+  return get_connection().hkeys("jargon-defs")
+
+def set_def(name, d):
+  return get_connection().hset("jargon-defs", name, json.dumps(d))
+
+if __name__ == "__main__":
+  DEBUG = True
+  print get_def("nothing") == None
+  set_def("test", {"test": 1})
+  print get_def("test")["test"] == 1
+  set_def("test", {"test": 2})
+  print get_def("test")["test"] == 2
+  set_def("test2", {"test": 0})
+  print get_all_def()
